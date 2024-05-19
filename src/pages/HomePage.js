cc71512be284b9d7a150d74e712020cd5f695df4
Kristof Bohanec <bohanec.kristof@gmail.com>
2024-05-19 15:00:09 +0200

    SPrememba za testiranje github actions workflow PMI-37

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 function HomePage() {
     return (
         <div>
-            <h1>CHARGING STATION GO BRUM BRUM tets</h1>
+            <h1>CHARGING STATION GO BRUM BRUM tetsa</h1>
         </div>
     )
 }
