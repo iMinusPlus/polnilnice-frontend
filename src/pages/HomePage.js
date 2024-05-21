f15bf2abde1f05b528d25ce2a73736ef5fae1140
NucleusBeast <36455332+NucleusBeast@users.noreply.github.com>
2024-05-21 14:55:53 +0200

    PMI-43 Namestitev TailwindCSS in

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,7 @@ function HomePage() {
             <AppBar />
             <div className="jumbotron text-center page-content ">
                 <h1 className="display-4">CHARGING STATION GO BRUM BRUM</h1>
-                <p className="lead">Looking to charge your car?</p>
+                <p className="text-3xl font-bold underline">Looking to charge your car?</p>
                 <p className="lead">Look further here nothing is yet implemented</p>
                 <p className="display-6 lead error-text" style={styles.underConstruction}>PAGE UNDER CONSTRUCTION</p>
             </div>
