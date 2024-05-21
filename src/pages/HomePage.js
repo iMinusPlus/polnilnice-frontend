db7e5638219b20851da99387e8f4977c8e17fb69
Kristof Bohanec <bohanec.kristof@gmail.com>
2024-05-21 10:02:23 +0200

    Page under construction text na neimplemtiranih straneh PMI-36

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,22 @@ import AppBar from "../components/AppBar";
 
 function HomePage() {
     return (
-        <div>
-            <AppBar/>
-            <h1>CHARGING STATION GO BRUM BRUM</h1>
+        <div className="container-fluid">
+            <AppBar />
+            <div className="jumbotron text-center page-content ">
+                <h1 className="display-4">CHARGING STATION GO BRUM BRUM</h1>
+                <p className="lead">Looking to charge your car?</p>
+                <p className="lead">Look further here nothing is yet implemented</p>
+                <p className="display-6 lead error-text" style={styles.underConstruction}>PAGE UNDER CONSTRUCTION</p>
+            </div>
         </div>
     )
+}
+
+const styles = {
+    underConstruction: {
+        marginTop: "10rem"
+    }
 }
 
 export default HomePage;
