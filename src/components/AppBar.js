b2fcb7bdaa7282eabd2b5227230dc1630ef03123
NucleusBeast <36455332+NucleusBeast@users.noreply.github.com>
2024-05-21 18:48:30 +0200

    PMI-47 Izpis polnilnic

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,23 +1,26 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function AppBar() {
     return (
-        <header className="AppBar">
+
             <div className="navbar bg-base-100 text-white ps-3 pe-4 shadow-md ">
-                <div className="">
+                <div className="navbar-start">
                     <Link className="navbar-brand" to="/">
                         <img src="/Elektropolnilnice_FullColor_NoBg_Symbol_2010x1080.png"
                              alt="Logo" className=" h-[50px]" />
                     </Link>
                 </div>
-                <div className="">
+                <div className="navbar-center">
                     <div><Link className="btn text-white" to="/">Home</Link></div>
                     <div ><Link className="btn text-white" to="/user">User</Link></div>
+                    <div ><Link className="btn text-white" to="/polnilnice">Polnilnice</Link></div>
+                </div>
+                <div className={"navbar-end"}>
+                    <p>1</p>
                 </div>
             </div>
-        </header>
+
     );
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,17 +7,11 @@ function HomePage() {
             <div className="jumbotron text-center page-content text-white">
                 <h1 className="display-4">CHARGING STATION GO BRUM BRUM</h1>
                 <p className="text-3xl font-bold underline">Looking to charge your car?</p>
-                <p className="lead">Look further here nothing is yet implemented</p>
-                <p className="display-6 lead error-text" style={styles.underConstruction}>PAGE UNDER CONSTRUCTION</p>
+                <p className="text-primary">Look further here nothing is yet implemented</p>
+                <p className="display-6 lead text-error">PAGE UNDER CONSTRUCTION</p>
             </div>
         </div>
     )
-}
-
-const styles = {
-    underConstruction: {
-        marginTop: "10rem"
-    }
 }
 
 export default HomePage;
