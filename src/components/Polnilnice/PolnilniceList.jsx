a6f5749e5d6307fa43df84675116113c0152524a
NucleusBeast <filip.skrget1@gmail.com>
2024-05-23 21:15:12 +0200

    Mogoče rešitev za crashe? PMI-56

--- src/components/Polnilnice/PolnilniceList.jsx
+++ src/components/Polnilnice/PolnilniceList.jsx
@@ -19,26 +19,21 @@ function PolnilniceList() {
     }, []);
 
     if (isLoading) {
-        return <div>
+        return (<div>
             <AppBar/>
             <div>
                 Loading...
             </div>
-        </div>;
-        return <div>Loading...</div>;
+        </div>);
     }
 
-    return (
-        <div className="mx-auto bg-neutral">
+    return (<div className="mx-auto bg-neutral">
             <AppBar/>
             <h1 className="text-4xl font-bold mb-4 mt-4 text-white">Polnilnice:</h1>
             <div className="grid grid-cols-4 gap-4 mr-6 bg-neutral">
-                {data.map((item, index) => (
-                    <PolnilnicaCard key={index} item={item}/>
-                ))}
+                {data.map((item, index) => (<PolnilnicaCard key={index} item={item}/>))}
             </div>
-        </div>
-    );
+        </div>);
 }
 
 export default PolnilniceList;
