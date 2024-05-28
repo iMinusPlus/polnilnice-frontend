a89e496b7c315c897d5cbef7a7ec4e28c99987f7
davidgole1 <david.gole1@student.um.si>
2024-05-28 15:38:59 +0200

    PMI-25 Sistem za prijavo. Sedaj se token shrani v local storage in odsifrira se ga v backendu preko funkcije protected. Nato se shrani v state userData, ta pa v userContext

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,15 +1,37 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import {UserContext} from "../context/userContext";
+import React, { useEffect, useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../context/userContext";
 
 function AppBar() {
-    const {user} = UserContext;
-    return (
+    const [token, setToken] = useState('')
+    const [userData, setUserData] = useState({})
+    const userContext = useContext(UserContext);
 
+    useEffect(() => {
+        const storedToken = localStorage.getItem('jwt-token');
+        if (storedToken) {
+            setToken(storedToken);
+            fetch('http://elektropolnilnice.eu:3000/users/protected', {
+                headers: {
+                    'jwt-token': storedToken,
+                },
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    setUserData(data);
+                    userContext.setUserContext(data); // Shrani podatke v UserContext
+                })
+                .catch((err) => {
+                    console.error('Error fetching protected route:', err);
+                });
+        }
+    }, []);
+
+    return (
         <div className="navbar bg-base-100 text-white shadow-md">
             <div className="navbar-start">
                 <Link className="navbar-brand" to="/">
-                    <img src="/Elektropolnilnice_FullColor_NoBg_Symbol_2010x1080.png" alt="Logo" className="h-[50px]"/>
+                    <img src="/Elektropolnilnice_FullColor_NoBg_Symbol_2010x1080.png" alt="Logo" className="h-[50px]" />
                 </Link>
             </div>
             <div className="navbar-center hidden lg:flex">
@@ -20,30 +42,28 @@ function AppBar() {
                 </div>
             </div>
             <div className="navbar-end hidden lg:flex gap-2">
-                {!user && (
+                {!token && (
                     <div className="join">
                         <div><Link className="btn text-white join-item" to="/login">Login</Link></div>
                         <div><Link className="btn text-white join-item" to="/register">Register</Link></div>
                     </div>
                 )}
-                {user && (
+                {token && (
                     <div className="dropdown dropdown-end">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
-                                <img alt="Tailwind CSS Navbar component"
-                                     src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
+                                <img alt="User Avatar" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                             </div>
                         </div>
-                        <ul tabIndex={0}
-                            className="mt-3 z-50 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="mt-3 z-50 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
                             <li>
-                                <Link className="justify-between" to={"/"}>
+                                <Link className="justify-between" to="/">
                                     Profile
                                     <span className="badge">New</span>
                                 </Link>
                             </li>
-                            <li><Link to={"/"}>Settings</Link></li>
-                            <li><Link to={"/"}>Logout</Link></li>
+                            <li><Link to="/">Settings</Link></li>
+                            <li><Link to="/">Logout</Link></li>
                         </ul>
                     </div>
                 )}
@@ -51,18 +71,15 @@ function AppBar() {
             <div className="navbar-end lg:hidden flex-grow">
                 <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                  d="M4 6h16M4 12h16m-7 6h7"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
                         </svg>
                     </label>
-                    <ul tabIndex={0}
-                        className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 z-50">
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 z-50">
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/user">User</Link></li>
                         <li><Link to="/polnilnice">Polnilnice</Link></li>
-                        {!user && (
+                        {!token && (
                             <>
                                 <li><Link to="/login">Login</Link></li>
                                 <li><Link to="/register">Register</Link></li>
@@ -70,32 +87,27 @@ function AppBar() {
                         )}
                     </ul>
                 </div>
-                {user && (
+                {token && (
                     <div className="dropdown dropdown-end">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
-                                <img alt="Tailwind CSS Navbar component"
-                                     src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
+                                <img alt="User Avatar" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                             </div>
                         </div>
-                        <ul tabIndex={0}
-                            className="mt-3 z-50 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="mt-3 z-50 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
                             <li>
-                                <Link className="justify-between" to={"/"}>
+                                <Link className="justify-between" to="/">
                                     Profile
                                     <span className="badge">New</span>
                                 </Link>
                             </li>
-                            <li><Link to={"/"}>Settings</Link></li>
-                            <li><Link to={"/"}>Logout</Link></li>
+                            <li><Link to="/">Settings</Link></li>
+                            <li><Link to="/">Logout</Link></li>
                         </ul>
                     </div>
                 )}
-
             </div>
         </div>
-
-
     );
 }
 
