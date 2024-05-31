9dfdeeb9141f107c7c547067b93f76bb22a1cf37
NucleusBeast <filip.skrget1@gmail.com>
2024-05-31 09:50:26 +0200

    Prikaz točk na zmeljevidu PMI-61

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,7 +5,7 @@ import Footer from "../components/Footer";
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibnVjbGV1c2JlYXN0IiwiYSI6ImNsd2pvandmcjE0ZTEyaW83ajMwdDd3NHQifQ.3v2gWxIh8fwbkupLAabx0A';
 
-function HomePage(props) {
+function HomePage() {
 
     const mapContainer = useRef(null);
     const map = useRef(null);
@@ -21,7 +21,41 @@ function HomePage(props) {
             center: [lng, lat],
             zoom: zoom
         });
-    });
+
+        // Fetch data from backend
+        fetch('http://elektropolnilnice.eu:3000/address/')
+            .then(response => response.json())
+            .then(data => {
+                const coordinates = data.map(item => ({
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [item.longitude, item.latitude]
+                    }
+                }));
+
+                // Wait for the map to load before adding the source and layer
+                map.current.on('load', function () {
+                    map.current.addSource('points', {
+                        'type': 'geojson',
+                        'data': {
+                            'type': 'FeatureCollection',
+                            'features': coordinates
+                        }
+                    });
+
+                    map.current.addLayer({
+                        'id': 'points',
+                        'type': 'circle',
+                        'source': 'points',
+                        'paint': {
+                            'circle-radius': 10,
+                            'circle-color': '#007cbf'
+                        }
+                    });
+                });
+            });
+    }, []);
 
     return (
         <div className="bg-neutral min-h-screen flex flex-col">
