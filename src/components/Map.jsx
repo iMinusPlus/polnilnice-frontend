278a0c671df007a43a712f20470dde74355d77d8
NucleusBeast <filip.skrget1@gmail.com>
2024-05-31 10:06:29 +0200

    Premestitev zemljevida v svojo komponento Map PMI-61

---
+++ src/components/Map.jsx
@@ -0,0 +1,96 @@
+import React, {useEffect, useRef, useState} from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibnVjbGV1c2JlYXN0IiwiYSI6ImNsd2pvandmcjE0ZTEyaW83ajMwdDd3NHQifQ.3v2gWxIh8fwbkupLAabx0A';
+
+function Map() {
+
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng] = useState(15.655);
+    const [lat] = useState(46.5555);
+    const [zoom] = useState(13);
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat],
+            zoom: zoom
+        });
+
+        // Fetch data from backend
+        fetch('http://elektropolnilnice.eu:3000/address/')
+            .then(response => response.json())
+            .then(data => {
+                const coordinates = data.map(item => ({
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [item.longitude, item.latitude]
+                    }
+                }));
+
+                // Wait for the map to load before adding the source and layer
+                map.current.on('load', function () {
+                    map.current.addSource('points', {
+                        'type': 'geojson',
+                        'data': {
+                            'type': 'FeatureCollection',
+                            'features': coordinates
+                        }
+                    });
+
+                    map.current.addLayer({
+                        'id': 'points',
+                        'type': 'circle',
+                        'source': 'points',
+                        'paint': {
+                            'circle-radius': 10,
+                            'circle-color': '#007cbf'
+                        }
+                    });
+
+                    // Create a popup, but don't add it to the map yet.
+                    var popup = new mapboxgl.Popup({
+                        closeButton: false,
+                        closeOnClick: false
+                    });
+
+                    map.current.on('mouseenter', 'points', function (e) {
+                            // Change the cursor style as a UI indicator.
+                            map.current.getCanvas().style.cursor = 'pointer';
+
+                            var coordinates = e.features[0].geometry.coordinates.slice();
+                            var description = e.features[0].properties.description;
+
+                            // Ensure that if the map is zoomed out such that multiple
+                            // copies of the feature are visible, the popup appears
+                            // over the copy being pointed to.
+                            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+                            }
+
+                            // Populate the popup and set its coordinates
+                            // based on the feature found.
+                            popup.setLngLat(coordinates).setHTML(description).addTo(map.current);
+                        }
+                    );
+
+                    map.current.on('mouseleave', 'points', function () {
+                            map.current.getCanvas().style.cursor = '';
+                            popup.remove();
+
+                        }
+                    );
+                });
+            });
+    }, []);
+
+    return (
+        <div ref={mapContainer} className="map-container"/>
+    );
+}
+
+export default Map;
